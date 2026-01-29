f38222e06b9940c8090f7668a8c97acc8daa6e02
AteszXD <keria@kkszki.hu>
2026-01-29 13:06:32 +0100

    Szolgáltató feltöltő függvény

--- KockasFuzet/Controllers/SzolgaltatoController.cs
+++ KockasFuzet/Controllers/SzolgaltatoController.cs
@@ -31,5 +31,26 @@ namespace KockasFuzet.Controllers
             connection.Close();
             return szolgaltatok;
         }
+
+        public string CreateSzolgaltato(Szolgaltato szolgaltato)
+        {
+            MySqlConnection connection = new MySqlConnection();
+            string connectionString = "SERVER=localhost;DATABASE=kockasfuzet;UID=root;PASSWORD=;";
+            connection.ConnectionString = connectionString;
+            connection.Open();
+
+            string cmd = "INSERT INTO `szolgaltato`(`RovidNev`, `Nev`, `Ugyfelszolgalat`) VALUES (@RovidNev,@Nev,@Ugyfelszolgalat)";
+            MySqlCommand command = new MySqlCommand(cmd, connection);
+
+            command.Parameters.AddWithValue("@Rovidnev", szolgaltato.RovidNev);
+            command.Parameters.AddWithValue("@Nev", szolgaltato.Nev);
+            command.Parameters.AddWithValue("@Ugyfelszolgalat", szolgaltato.Ugyfelszolgalat);
+
+            int sorokSzama = command.ExecuteNonQuery();
+            connection.Close();
+
+            string valasz = sorokSzama > 0 ? "Sikeres rögzítés" : "Sikertelen rögzítés";
+            return valasz;
+        }
     }
 }
